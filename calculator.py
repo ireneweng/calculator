ad7fd3265c4c75ca542a457198417e462b2e0407
Ran Weng <ireneylzr@gmail.com>
2023-10-13 12:24:08 -0400

    fix issues with negatives and pemdas

--- calculator.py
+++ calculator.py
@@ -28,8 +28,50 @@ class Calculator(object):
                 break
         return input[start:end]
 
+    def resolve_negatives(self, expr_list: list[str]) -> list[str]:
+        """
+        Resolves negative numbers in expression list.
+        Ex. input "7/-4-3*-2" as list:
+                -> ['7', '/', '', '-', '4', '-', '3', '*', '', '-', '2']
+                -> ['7', '/', '-4', '-', '3', '*', '-2']
+        """
+        for i, val in enumerate(expr_list):
+            if not val:
+                neg = expr_list[i + 1] + expr_list[i + 2]
+                expr_list[i] = neg
+                del expr_list[i + 1 : i + 3]
+        LOG.debug(f"Resolved negatives: {expr_list}")
+        return expr_list
+
+    def resolve_mul_div(self, expr_list: list[str]) -> list[str]:
+        """
+        Recursively solves the multiplication/division parts of an equation.
+        Returns the expression list with only addition/subtraction left.
+        Ex. input "7/-4-3*-2" as list:
+                -> ['7', '/', '-4', '-', '3', '*', '-2']
+                -> [-1.75, '-', -6.0]
+        """
+        for i, val in enumerate(expr_list):
+            if val in ["*", "/"]:
+                sol = self.compute_expression(expr_list[i - 1 : i + 2])
+                expr_list = expr_list[: i - 1] + [sol] + expr_list[i + 2 :]
+                break
+        while "*" in expr_list or "/" in expr_list:
+            LOG.debug(f"Resolving mul/div: {expr_list}")
+            expr_list = self.resolve_mul_div(expr_list)
+        return expr_list
+
     def compute_expression(self, expr_list: list[str]) -> float:
-        """Recursively computes a base expression from left to right."""
+        """
+        Recursively computes an expression from left to right.
+        NOTE: Does not follow order of operations; use resolve_mul_div() first.
+        Ex. input "7/-4-3":
+                -> ['7', '/', '-4', '-', '3']
+                -> -4.75
+        Ex. input "7/-4-3*-2" ignores PEMDAS:
+                -> ['7', '/', '-4', '-', '3', '*', '-2']
+                -> 9.5
+        """
         if len(expr_list) == 1:
             return float(expr_list[0])
         l_expr = expr_list[:3]
@@ -37,6 +79,7 @@ class Calculator(object):
         lval, rval = float(l_expr[0]), float(l_expr[2])
         op = l_expr[1]
         if op in self.operator_list:
+            LOG.debug(f"Sub-equation: {expr_list}")
             match op:
                 case "+":
                     result = lval + rval
@@ -54,21 +97,14 @@ class Calculator(object):
         while any(op in expression for op in self.operator_list):
             base_expr = self.get_base_expression(expression)
             base_stripped = self.strip_parens(base_expr)
-            base_list = re.split("([\-\+])", base_stripped)
-            for i, val in enumerate(base_list):
-                if not val:
-                    neg = base_list[i + 1] + base_list[i + 2]
-                    base_list[i] = neg
-                    del base_list[i + 1 : i + 3]
-            for i, val in enumerate(base_list):
-                if "/" in val or "*" in val:
-                    exp_list = re.split("([\/\*])", val)
-                    result = self.compute_expression(exp_list)
-                    base_list[i] = result
+            base_list = re.split("([\/\*\-\+])", base_stripped)
+            base_list = self.resolve_negatives(base_list)
+            base_list = self.resolve_mul_div(base_list)
             result = self.compute_expression(base_list)
             expression = expression.replace(base_expr, str(result))
-            LOG.debug(f"Expr: {expression}")
-        return float(expression)
+            if len(base_list) == 1:
+                break
+        return result
 
     def run(self, input: str) -> None:
         """Runs the calculator."""
